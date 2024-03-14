6542405241c2cad93d58ddc4c43707675ce98f6b
NESHGP04 <89556682+NESHGP04@users.noreply.github.com>
2024-03-14 11:49:29 -0600

    HashTest terminado

---
+++ src/Hashtest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Hashtest {
+    @Test
+    public void testHashMD5() {//MD5
+        String str = "Kevin es feo";
+        String expectedHash = "2023dc91c11bde69e9c26fcc95ac662a";
+
+        MD5HashMap  hashMap = new MD5HashMap();
+        String actualHash = hashMap.hash(str);
+
+        assertEquals(expectedHash, actualHash);
+    }
+
+    @Test
+    public void testHashorganico() {
+        String str = "Kevin es feo";
+        String expectedHash = "Kevin es feo";
+
+        OrganicoHashMap  hashMap = new OrganicoHashMap();
+        String actualHash = hashMap.hash(str);
+
+        assertEquals(expectedHash, actualHash);
+    }
+
+    @Test
+    public void testHashSHA1(){
+        String str = "Kevin es feo";
+        String expectedHash = "973ec79071f9b63ceaa3f3806d9013705422d414";
+
+        SHA1HashMap  hashMap = new SHA1HashMap();
+        String actualHash = hashMap.hash(str);
+
+        assertEquals(expectedHash, actualHash);
+    }
+}
