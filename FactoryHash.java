a0c8e6b852043214886d0a871cfd930c25eced03
NESHGP04 <89556682+NESHGP04@users.noreply.github.com>
2024-03-14 11:17:13 -0600

    Interfaz, clases y factory Hash creadas

---
+++ FactoryHash.java
@@ -0,0 +1,17 @@
+package src;
+public class FactoryHash<K,V> {
+
+    public static final int MD5_HASH = 0;
+    public static final int SHA1_HASH = 1;
+    public static final int ORGANICO_Hash = 2;
+
+    public static IHash getHashMap (int formatType){
+        switch(formatType) {
+            case MD5_HASH: return new MD5HashMap();
+            case SHA1_HASH: return new SHA1HashMap();
+            case ORGANICO_Hash: return new OrganicoHashMap();
+            default : return new MD5HashMap();
+        }
+    }
+    
+}
---
+++ IHash.java
@@ -0,0 +1,4 @@
+package src;
+public interface IHash {
+    String hash(String input);
+}
